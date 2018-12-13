509f0c41ad48bc79b955f0250629b4f3ef89b23a
yuyuforest <yumlin16@outlook.com>
2018-12-13 17:18:33 +0800

    Remove already_login and not_login

--- app/config.go
+++ app/config.go
@@ -4,5 +4,6 @@ import "github.com/XMatrixStudio/Violet.SDK.Go"
 
 // Config 配置文件
 type Config struct {
-	Violet violetSdk.Config `yaml:"Violet"` // Violet配置
+	Violet	violetSdk.Config	`yaml:"Violet"` // Violet配置
+	Port	int					`yaml:"Port"`	// 端口
 }
